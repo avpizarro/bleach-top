51128ffd3f2e3a0b0872104d0ca2526266cc1662
Alejandra Valdes Pizarro <avaldespizarro@outlook.com>
2025-02-08 20:42:42 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useRef, Suspense } from 'react';
+import { OrbitControls } from '@react-three/drei';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import './App.css';
 
-function App() {
+import background from './_C3A9430.jpg';
+
+function Top()
+{
+  const ref = useRef();
+  const Gltf = useLoader(GLTFLoader, '/mesh top.gltf');
+  useFrame(() =>
+  {
+    ref.current.rotation.y += 0.001;
+  })
+  console.log(Gltf);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <primitive
+      ref={ref}
+      object={Gltf.scene}
+      position={[0, -11, 0]}
+      scale={10} />
+  )
 }
+
+function Scene(props)
+{
+  return (
+    <>
+      <ambientLight intensity={2} />
+      <Suspense fallback={null}>
+        <Top />
+      </Suspense>
+      <OrbitControls maxDistance={20} minDistance={8} />
+    </>
+  );
+};
+
+function App()
+{
+  return (
+    <>
+      <div style={{ backgroundImage: `url(${background})` }}>
+
+        <h1>Bleach</h1>
+
+        <Canvas
+          camera={{
+            position: [0, 0, 2.2],
+          }}
+          style={{ height: '100vh' }}
+        >
+
+          <Scene />
+
+        </Canvas>
+
+      </div>
+    </>
+  );
+};
 
 export default App;
